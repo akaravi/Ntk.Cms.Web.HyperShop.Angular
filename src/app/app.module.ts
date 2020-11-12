99b0aa6678f737b7aeaa7dc1f4cae8544176462d
karavi home <offic-karavi@ntk.ir>
2020-11-12 14:02:02 +0330

    change to run

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { Inject, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { ToastrModule } from 'ngx-toastr/toastr/toastr.module';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { SharedModule } from './shared/shared.module';
 import { CoreAuthService, EnumDeviceType, EnumOperatingSystemType, TokenDeviceClientInfoDtoModel } from 'ntk-cms-api';
+import { DOCUMENT } from '@angular/common';
+
 
 @NgModule({
   declarations: [
@@ -21,17 +22,20 @@ import { CoreAuthService, EnumDeviceType, EnumOperatingSystemType, TokenDeviceCl
     SharedModule
   ],
   providers: [
+    ToastrService,
     CoreAuthService
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 
-  constructor(private coreAuthService: CoreAuthService) {
+  constructor(private coreAuthService: CoreAuthService, @Inject(DOCUMENT) private document: Document) {
     // karavi:Important For Test To Local Service
     if (environment.cmsServerConfig.configApiServerPath && environment.cmsServerConfig.configApiServerPath.length > 0) {
       this.coreAuthService.setConfig(environment.cmsServerConfig.configApiServerPath);
     }
+    const domain = this.document.location.hostname;
+    console.log('domain:', domain);
     const DeviceToken = this.coreAuthService.getDeviceToken();
     if (!DeviceToken || DeviceToken.length === 0) {
       const model: TokenDeviceClientInfoDtoModel = {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
+import { EnumDeviceType, EnumOperatingSystemType } from 'ntk-cms-api';
+
 export const environment = {
-  production: true
+  production: true,
+  cmsServerConfig : {
+    configApiRetry: 1,
+    configApiServerPath: 'https://apicms.ir/api/v1/',
+    configRouteThumbnails: 'https://oco.ir/imageThumbnails/',
+    configRouteUploadFileContent: 'https://apicms.ir/api/v1/FileContent/upload/',
+  },
+  cmsTokenConfig : {
+    SecurityKey: 'qwp123456',
+    ClientMACAddress: '',
+    OSType: EnumOperatingSystemType.Windows,
+    DeviceType: EnumDeviceType.WebSite,
+    PackageName: '',
+  }
 };
